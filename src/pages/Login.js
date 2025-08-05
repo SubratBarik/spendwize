8f043aeb6549104f71752749ad50d637762bb731
SubratBarik <subrat@blockstack.tech>
2025-08-05 20:32:43 +0530

    Login css updated

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
 function Login() {
     return (
         <>
             <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
                 <div className="card p-4 shadow" style={{ minWidth: '400px', border: '1px solid #ced4da' }}>
-                    <h2 className="mb-4 text-center">SpendWize</h2>
+                    <h2 className="mb-4 text-center">Spend Wize</h2>
                     <form>
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email:</label>
